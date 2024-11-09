cf3cff3d812fab01e304ad64939d9773c226a836
Michael <jenifergunawan5@gmail.com>
2024-11-09 23:21:25 +0700

    create project wisata_candi

---
+++ wisata_candi/lib/data/candi_data.dart
@@ -0,0 +1,20 @@
+import 'package:wisata_candi/models/candi.dart';
+
+var candiList = [
+  Candi (
+    name : 'Candi Borobudur',
+    location : 'Magelang, Jawa Tengah',
+    description:
+      'Candi Borobudur diketahui merupakan peninggalan sarana ritual agama Buddha, tepatnya perpaduan ajaran Buddha Mahayana dan Tantrayana dengan meditasi filsafat Yogacara. Ajaran Buddha semacam itu disebut mirip dengan yang berkembang di Bengal, India pada abad ke-8 pada masa pemerintahan raja-raja Pala. ',
+    built: 'Abad ke-9',
+    type : 'Candi Buddha',
+    imageAsset: 'images/borobudur.jpg',
+    imageUrls: [
+      'https://dhammadiparama.com/wp-content/uploads/2022/07/Candi-Borobudur-scaled.jpg',
+      'https://pict.sindonews.net/dyn/850/pena/news/2024/05/03/29/1370163/jejak-rekam-keajaiban-candi-borobudur-dari-sang-maestro-raja-sriwijaya-samaratungga-vgx.jpg',
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.indonesia.travel%2Fid%2Fid%2Fdestinasi%2Fjava%2Fmagelang-regency%2Fborobudur.html&psig=AOvVaw3Tsjbe0gX5Fli_MSLuE0xc&ust=1731254829780000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNCO8_rQz4kDFQAAAAAdAAAAABAh',
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.liputan6.com%2Fhot%2Fread%2F4157213%2Fwisata-candi-borobudur-dan-destinasi-menarik-di-sekitarnya&psig=AOvVaw3Tsjbe0gX5Fli_MSLuE0xc&ust=1731254829780000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNCO8_rQz4kDFQAAAAAdAAAAABAp',
+    ],
+    isFavorite : false,
+  ),
+];
