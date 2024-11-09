3226af082275d4a85a736c128d00ca99cabb6c79
Michael <jenifergunawan5@gmail.com>
2024-11-09 23:58:50 +0700

    update main.dart

--- wisata_candi/lib/data/candi_data.dart
+++ wisata_candi/lib/data/candi_data.dart
@@ -12,9 +12,27 @@ var candiList = [
     imageUrls: [
       'https://dhammadiparama.com/wp-content/uploads/2022/07/Candi-Borobudur-scaled.jpg',
       'https://pict.sindonews.net/dyn/850/pena/news/2024/05/03/29/1370163/jejak-rekam-keajaiban-candi-borobudur-dari-sang-maestro-raja-sriwijaya-samaratungga-vgx.jpg',
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.indonesia.travel%2Fid%2Fid%2Fdestinasi%2Fjava%2Fmagelang-regency%2Fborobudur.html&psig=AOvVaw3Tsjbe0gX5Fli_MSLuE0xc&ust=1731254829780000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNCO8_rQz4kDFQAAAAAdAAAAABAh',
+      'https://upload.wikimedia.org/wikipedia/commons/7/77/Stupa_Borobudur.jpg',
       'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.liputan6.com%2Fhot%2Fread%2F4157213%2Fwisata-candi-borobudur-dan-destinasi-menarik-di-sekitarnya&psig=AOvVaw3Tsjbe0gX5Fli_MSLuE0xc&ust=1731254829780000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNCO8_rQz4kDFQAAAAAdAAAAABAp',
     ],
     isFavorite : false,
   ),
+  Candi (
+    name : 'Candi Mendut',
+    location : 'Magelang, Jawa Tengah',
+    description:
+      ' Candi Mendut merupakan candi Buddha yang memiliki bentuk persegi dengan atap bertingkat. Di dalamnya terdapat arca Buddha besar yang dianggap sangat sakral oleh masyarakat sekitar. Relief pada dinding candi menggambarkan kisah-kisah ajaran Buddha, termasuk cerita-cerita binatang (fabel) yang mengandung pesan moral.',
+    built: 'Abad ke-9',
+    type : 'Candi Buddha',
+    imageAsset: 'images/mendut.jpg',
+    imageUrls: [
+      'https://id.m.wikipedia.org/wiki/Berkas:Mendut_Temple.jpg',
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bakpiamutiarajogja.com%2Fmengenal-lebih-dekat-wisata-candi-mendut-magelang%2F&psig=AOvVaw0RJU1YZs7viyi2rvDipr-7&ust=1731257091583000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKiRvrHZz4kDFQAAAAAdAAAAABBR',
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fsewabusjogja.co.id%2Fcandi-mendut-magelang-keindahan-warisan%2F&psig=AOvVaw0RJU1YZs7viyi2rvDipr-7&ust=1731257091583000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKiRvrHZz4kDFQAAAAAdAAAAABBh',
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fregional.kompas.com%2Fread%2F2023%2F06%2F03%2F183026078%2Fcandi-mendut-fungsi-sejarah-serta-relief-dan-arca%3Fpage%3Dall&psig=AOvVaw0RJU1YZs7viyi2rvDipr-7&ust=1731257091583000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKiRvrHZz4kDFQAAAAAdAAAAABBx',
+    ],
+    isFavorite : false,
+  ),
+
+
 ];
